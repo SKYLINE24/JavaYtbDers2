b2bcfb21b910c05da8b9064a0c97bc7a2ede42dd
Erbil Can Keleş <cankls2434@gmail.com>
2022-08-23 08:51:09 +0300

    kullanıcıdan array alma ve arrayin ortalamasını yazdırma

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void mean(int[] arr) {
+        int total = 0;
+        for(int i = 0; i < arr.length; i++){
+            total += arr[i];
+        }
+        System.out.println("Ortalama: " + (double)total / arr.length);
+    }
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //int[] arr1 = {10,20,30,40,50};
+        int[] arr2 = new int [5];
+
+        //kullanıcıdan array oluşturmasını istiyoruz
+        System.out.println("Array2'nin değerlerini girin...");
+        for(int i = 0; i < arr2.length; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        /*
+        //array elemanlarını yazdırmak istersek
+        for(int i = 0; i < arr2.length; i++){
+            System.out.println("eleman : " + arr2[i]);
+        }
+        */
+        //mean methodunun içerisinne arr2 arrayimizi göndermiş ve sonrasında da ortalamasını almış olduk
+        mean(arr2);
+
+        /*
+        arr2[0] = 1;
+        arr2[1] = 2;
+        arr2[2] = 3;
+        arr2[3] = 4;
+        arr2[4] = 5;
+
+        System.out.println("Array 2'nin birinci elemeanı: " + arr2[0]);
+        System.out.println("Array 1'in ikinci elemeanı: " + arr1[1]);
+        */
+
+
+
+    }
+
 }
